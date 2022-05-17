a4332c8dc59edf262166d584cfea503842527fd5
Vlad <colacat1927@gmail.com>
2022-05-17 16:48:42 +0300

    update product routes

--- routes/product.js
+++ routes/product.js
@@ -1,8 +1,18 @@
 import express from 'express';
-import { createProduct } from '../controllers/product.js';
+import {
+    createProduct,
+    deleteProduct,
+    getProductById,
+    getProducts,
+    updateProduct,
+} from '../controllers/product.js';
 
 const router = express.Router();
 
 router.post('/', createProduct);
+router.get('/', getProducts);
+router.get('/:id', getProductById);
+router.delete('/:id', deleteProduct);
+router.update('/:id', updateProduct);
 
 export default router;
